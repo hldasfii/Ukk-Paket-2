a963740aa785730ecd4d5b12ac33f75f43a1d08f
hldashfii <hildashofiana1512@gmail.com>
2023-03-08 08:50:50 +0700

    first

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Petugas;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Petugas::create([
+            'nama_petugas' => 'Hilda Shofi',
+            'username' => 'hldashfii',
+            'password' => Hash::make('password'),
+            'telp' => '081234567',
+            'level' => 'admin',
+        ]);
+    }
+}
